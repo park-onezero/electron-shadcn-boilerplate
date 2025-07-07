2d2e6d68ddda6dbef4e4717d22dd123d540c3cde
Kim <park.onezero@gmail.com>
2025-07-08 05:54:47 +0900

    feat: Tailwind CSS v4 설정 추가
    - Tailwind CSS v4.1.11 및 PostCSS 의존성 설치
    - PostCSS 설정으로 Tailwind 플러그인 구성
    - webpack 설정에 postcss-loader 추가
    - App.css에 Tailwind CSS import 추가
    - '@' 경로 별칭을 src/renderer로 설정

--- .erb/configs/webpack.config.base.ts
+++ .erb/configs/webpack.config.base.ts
@@ -46,6 +46,7 @@ const configuration: webpack.Configuration = {
     modules: [webpackPaths.srcPath, 'node_modules'],
     // There is no need to add aliases here, the paths in tsconfig get mirrored
     plugins: [new TsconfigPathsPlugins()],
+    alias: {'@':webpackPaths.srcRendererPath}
   },
 
   plugins: [new webpack.EnvironmentPlugin({ NODE_ENV: 'production' })],
